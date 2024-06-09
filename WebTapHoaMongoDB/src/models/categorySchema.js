5a41268536f6f888a292b3fb2031c03186363172
phandat1102 <150638834+phandat1102@users.noreply.github.com>
2024-06-09 17:12:19 +0700

    Delete WebTapHoaMongoDB directory

--- WebTapHoaMongoDB/src/models/categorySchema.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const cagtegorySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String },
-    image: { type: String}
-});
-
-module.exports = mongoose.model('Category', cagtegorySchema);
--- WebTapHoaMongoDB/src/models/groupSchema.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const groupSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String }
-});
-
-module.exports = mongoose.model('Group', groupSchema);
